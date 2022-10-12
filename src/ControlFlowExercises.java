6f596f6a810406253ba39658eaabfe3f771e7795
Richard Kim <richardjkim7@gmail.com>
2022-10-11 22:24:09 -0500

    display table of powers exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main (String[] args) {
 
@@ -30,18 +32,32 @@ public class ControlFlowExercises {
 //        } while (i < 1_000_000);
 
         //Fizzbuzz
-        for(int i = 1; i < 100; i++) {
-            if (i % 3 == 0) {
-                if (i % 5 == 0) {
-                    System.out.println("Fizzbuzz");
-                } else {
-                    System.out.println("Fizz");
-                }
-            } else if (i % 5 == 0){
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+//        for(int i = 1; i < 100; i++) {
+//            if (i % 3 == 0) {
+//                if (i % 5 == 0) {
+//                    System.out.println("Fizzbuzz");
+//                } else {
+//                    System.out.println("Fizz");
+//                }
+//            } else if (i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+        //Display table of powers
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What number would you like to go up to? ");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: " + userInput);
+
+        System.out.println("Here is your table!");
+        System.out.println();
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for (int i = 1; i <= userInput; i++) {
+            System.out.println(i + "      |" + " " + (i*i) + "       " + "|" + " " + (i*i*i));
         }
+
     }
 }
