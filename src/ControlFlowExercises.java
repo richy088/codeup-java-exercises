b14b95d98be9e5f590e6d019c13ea3f2fb2a35f7
Richard Kim <richardjkim7@gmail.com>
2022-10-11 20:54:01 -0500

    do-while loop exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,25 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        int i = 5;
-        while (i <= 15) {
+
+        //while loop that prints from 5-15
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//    }
+        //do-while loop that prints 0-100 evens
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i+=2;
+//        } while (i <= 100);
+
+        //do-while loop that prints from 100 to -10 decreasing by 5
+        int i = 100;
+        do {
             System.out.println(i);
-            i++;
-        }
+            i-=5;
+        } while (i >= -10);
     }
 }
