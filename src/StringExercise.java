fd867211bd9ba747a5d797a2ca4f3361579a0f1a
Richard Kim <richardjkim7@gmail.com>
2022-10-12 21:53:28 -0500

    string basics complete

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -4,5 +4,15 @@ public class StringExercise {
         String secondSentence = "We don't need no thought control";
         System.out.println(firstSentence);
         System.out.println(secondSentence);
+
+        // \" adds an exclamation mark wherever I want inside string
+        String thirdSentence = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(thirdSentence);
+
+        String fourthSentence = "In windows, the main drive is usually C:\\";
+        System.out.println(fourthSentence);
+
+        String fifthSentence = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(fifthSentence);
     }
 }
