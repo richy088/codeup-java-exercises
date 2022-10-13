4d927abf98160e369e87a9e9ae6701dbebb92572
Richard Kim <richardjkim7@gmail.com>
2022-10-12 22:51:18 -0500

    bob exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main (String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Say something to Bob: ");
+        String userInput = scanner.nextLine();
+        if (userInput.trim().endsWith("?")) {
+            System.out.println("Sure");
+        } else if (userInput.trim().endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        }
     }
 }
