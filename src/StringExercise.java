3343003ddcccee1cf57e9dd52bf4587de784189c
Richard Kim <richardjkim7@gmail.com>
2022-10-12 21:32:55 -0500

    Strings exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    public static void main (String[] args) {
+        String firstSentence = "We don't need no education";
+        String secondSentence = "We don't need no thought control";
+        System.out.println(firstSentence);
+        System.out.println(secondSentence);
+    }
+}
