7b6fb49ba4e2e713d37aa49942a1e2c90992be48
Richard Kim <richardjkim7@gmail.com>
2022-10-13 09:23:05 -0500

    methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,21 @@
 public class MethodsExercises {
     public static void main(String[] args) {
         //psvm + tab will create public static void main class
-
+        add(4, 3);
+        subtract(4, 3);
+        multiply(4, 3);
+        divide(4, 3);
+        }
+    public static void add(int firstNum, int secondNum) {
+        System.out.println(firstNum + secondNum);
+    }
+    public static void subtract(int firstNum, int secondNum) {
+        System.out.println(firstNum - secondNum);
+    }
+    public static void multiply(int firstNum, int secondNum) {
+        System.out.println(firstNum * secondNum);
+    }
+    public static void divide(int firstNum, int secondNum) {
+        System.out.println(firstNum / secondNum);
     }
 }
