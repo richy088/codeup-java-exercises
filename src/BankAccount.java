4a03167941f9de69994b46bd555e93eb9678d0a9
Richard Kim <richardjkim7@gmail.com>
2022-10-15 16:34:57 -0500

    java exercises

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -4,4 +4,31 @@ public class BankAccount {
      private String customerName;
      private String email;
      private int phoneNumber;
+
+     public void setBalance(int funds) {
+          this.balance = funds;
+     }
+
+     public int getBalance() {
+          return this.balance;
+     }
+
+     public void depositFunds(int deposit) {
+          this.balance += deposit;
+          System.out.println(this.balance);
+     }
+
+     public void withdrawFunds(int amount) {
+          if (this.balance < amount) {
+               System.out.println("Insufficient funds available");
+          } else {
+               this.balance -= amount;
+               System.out.println(this.balance);
+          }
+     }
+
+     public  void currentFunds() {
+          System.out.println("Your current balance is: $" + this.balance + " dollars");
+     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
 //        Car toyota = new Car();
 //        porsche.setModel("Honda");
 //        System.out.println("Model is: " + porsche.getModel());
+        BankAccount funds = new BankAccount();
+        funds.setBalance(1000);
+        System.out.println("Your balance is: $" + funds.getBalance() + " dollars");
+        funds.depositFunds(500);
+        funds.depositFunds(300);
+
+        funds.currentFunds();
+        funds.withdrawFunds(20);
+        funds.withdrawFunds(1800);
+        funds.withdrawFunds(500);
     }
 }
