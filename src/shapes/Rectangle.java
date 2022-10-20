f1cb2bd392709832be401ae391a8b8c0ca50f8ff
Richard Kim <richardjkim7@gmail.com>
2022-10-20 15:49:19 -0500

    polymorphism exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,36 @@
 package shapes;
 
 public class Rectangle {
+    private int length;
+    private int width;
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getArea(int length, int width) {
+        return length * width;
+    }
+
+    public int getPerimeter(int length, int width) {
+        return (2 * length) + (2 * width);
+    }
+
 }
